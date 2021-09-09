77ee63ddfdd6996042a75f3390c9add8eed8930f
Richard Gordon <richgordon95@gmail.com>
2021-09-08 21:15:36 -0400

    create project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Web3 from "web3";
+import Nav from "./components/Nav.js";
+import { simpleStorage } from "./abi/abi";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
 import './App.css';
 
+const web3 = new Web3(Web3.givenProvider);
+const contractAddress = "0x3CC583d276B548f644D47f95fe170C8DeE71C39c";
+const storageContract = new web3.eth.Contract(simpleStorage, contractAddress);
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+ }));
+
 function App() {
+  const classes = useStyles();
+  const [number, setUint] = useState(0);
+  const [getNumber, setGet] = useState("0");
+
+  const numberSet = async (t) => {
+    t.preventDefault();
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
+    const gas = await storageContract.methods.set(number).estimateGas();
+    const post = await storageContract.methods.set(number).send({
+      from: account,
+      gas,
+    });
+  };
+
+  const numberGet = async (t) => {
+    t.preventDefault();
+    const post = await storageContract.methods.get().call();
+    setGet(post);
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Nav />
+      <div className="main">
+        <div className="card">
+          <TextField
+            id="outlined-basic"
+            label="Set your uint256:"
+            onChange={(t) => setUint(t.target.value)}
+            variant="outlined"
+          />
+          <form className="form" onSubmit={numberSet}>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              value="Confirm"
+            >
+              Confirm
+            </Button>
+            <br />
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={numberGet}
+              type="button"
+            >
+              Get your uint256
+            </Button>
+            {getNumber}
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
+ }
 
 export default App;
---
+++ src/abi/abi.js
@@ -0,0 +1,47 @@
+export const simpleStorage = [
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "storedData",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "x",
+          "type": "uint256"
+        }
+      ],
+      "name": "set",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "get",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
